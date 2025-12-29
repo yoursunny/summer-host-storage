8336f1384dc639bc8d7ca644530ec5d7a8ed0d40
Junxiao Shi <git@mail1.yoursunny.com>
2025-12-29 22:50:40 +0000

    main: command line skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,39 @@
 use std::io;
-use yoursunny_summer_host_storage::{BitCounts, download};
+use yoursunny_summer_host_storage::{BitCounts, download, upload};
+
+use clap::{Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+#[command(name = "yoursunny_summer_host_storage")]
+#[command(about = "Deep Atlantic Storage app")]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    #[command(about = "Upload a file in offline mode")]
+    Upload {},
+    #[command(about = "Download a file in offline mode")]
+    #[command(arg_required_else_help = true)]
+    Download { cnt0: usize, cnt1: usize },
+}
 
 fn main() {
-    let counts = BitCounts { cnt0: 17, cnt1: 15 };
-    download(io::stdout(), &counts).unwrap();
+    let args = Cli::parse();
+
+    match args.command {
+        Commands::Upload {} => {
+            let counts = upload(io::stdin()).unwrap();
+            println!("{:} {:}", counts.cnt0, counts.cnt1)
+        }
+        Commands::Download { cnt0, cnt1 } => {
+            let counts = BitCounts {
+                cnt0: cnt0,
+                cnt1: cnt1,
+            };
+            download(io::stdout(), &counts).unwrap();
+        }
+    }
 }
